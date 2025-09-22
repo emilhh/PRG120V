2c6c4c66d4936372a98f9192f8ce7db84e31bfe2
emilhh <99885512+emilhh@users.noreply.github.com>
2025-09-22 11:35:58 +0200

    mnb

--- tema02/db-tilkobling.php
+++ tema02/db-tilkobling.php
@@ -3,9 +3,9 @@
 /* Programmet foretar tilkobling til database-server og valg av database
 */
 $host = 'b-studentsql-1.usn.no';
-$username = getenv('274645');
-$password = getenv('161b274645');
-$database = getenv('274645');
+$username = '274645';
+$password = '161b274645';
+$database = '274645';
 $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
 /* tilkobling til database-serveren utført */
 ?>
