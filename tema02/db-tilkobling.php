352e7997c5810a9f86905dd85e16b15276e4b0bf
emilhh <99885512+emilhh@users.noreply.github.com>
2025-09-22 11:08:20 +0200

    fhgjk

--- tema02/db-tilkobling.php
+++ tema02/db-tilkobling.php
@@ -2,10 +2,10 @@
 /*
 /* Programmet foretar tilkobling til database-server og valg av database
 */
-$host = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
+$host = getenv('274645@158.36.151.20');
+$username = getenv('274645');
+$password = getenv('161b274645');
+$database = getenv('274645');
 $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
 /* tilkobling til database-serveren utført */
 ?>
